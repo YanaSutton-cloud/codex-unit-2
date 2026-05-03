502d0aa476273426deb08a022004a19c0dfb7911
kali-red-tylbf <yana.sutton@ace.tamut.edu>
2026-05-03 04:09:47 -0500

    Finished Assignment

--- lesson-05-terminal-commands/level-03/answer.js
+++ lesson-05-terminal-commands/level-03/answer.js
@@ -1,3 +1,3 @@
 // Level 03 — Change directory with cd
 // TODO: Use `cd` to move into a folder and back; describe your commands here.
-export default "";
+export default "My terminal is not working properly when I enter cd to get into a folder.";
