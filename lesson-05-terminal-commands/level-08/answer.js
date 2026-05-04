49918068f03cf0a2501d04a589ca014e08ca9ed5
kali-red-tylbf <yana.sutton@ace.tamut.edu>
2026-05-04 02:17:43 -0500

    Ran Test - Completed the Assignment

--- lesson-05-terminal-commands/level-08/answer.js
+++ lesson-05-terminal-commands/level-08/answer.js
@@ -1,3 +1,3 @@
 // Level 08 — Clear the terminal
 // TODO: Run `clear` or press Ctrl+L and write a short string mentioning `clear` or `ctrl+l`.
-export default "";
+export default "I used the command 'clear' for this assignment.";
