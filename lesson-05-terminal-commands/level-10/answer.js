a80f1e8bb75ddc555be0d478e3153a36e08f8a5c
kali-red-tylbf <yana.sutton@ace.tamut.edu>
2026-05-04 04:57:05 -0500

    Ran Test -Completed Assignment

--- lesson-05-terminal-commands/level-10/answer.js
+++ lesson-05-terminal-commands/level-10/answer.js
@@ -1,3 +1,10 @@
 // Level 10 — Challenge: Multi-step script run
 // TODO: Export a string array with the VS Code steps and terminal commands you used.
-export default [];
+export default [
+  "created folder",
+  "added file",
+  "moved file",
+  "node ./folder/script.js",
+  "clear",
+  "cd ..",
+];
