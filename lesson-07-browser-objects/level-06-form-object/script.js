f6cb1af7d49055687a6cca513b963228b65a5ae1
kali-red-tylbf <yana.sutton@ace.tamut.edu>
2026-05-04 09:39:26 -0500

    Finished Assignment - Ran Test

--- lesson-07-browser-objects/level-06-form-object/script.js
+++ lesson-07-browser-objects/level-06-form-object/script.js
@@ -1,6 +1,12 @@
 // Level 06 — form-object starter
 // TODO: select the form element (e.g. document.getElementById('sample-form'))
+const form = document.getElementById("sample-form");
+
 // TODO: log form.id, form.name, form.elements.length, form.enctype, form.className
+console.log(form.id);
+console.log(form.name);
+console.log(form.nextElementSibling.length);
+console.log(form.enctype);
 
 // Scaffold:
 // const form = document.getElementById('sample-form');
