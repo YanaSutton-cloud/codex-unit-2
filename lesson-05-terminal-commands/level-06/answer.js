846876f7da7c5a441de878177f8fdcb63ced8308
kali-red-tylbf <yana.sutton@ace.tamut.edu>
2026-05-04 01:56:31 -0500

    Ran Test - Completed Assignment

--- lesson-05-terminal-commands/level-06/answer.js
+++ lesson-05-terminal-commands/level-06/answer.js
@@ -1,3 +1,3 @@
 // Level 06 — Run Node.js files with node
 // TODO: Run `node hello.js` and write a short note here mentioning `node`.
-export default "";
+export default "I ran node hello.js and the code said the MODULE_NOT_FOUND.";
