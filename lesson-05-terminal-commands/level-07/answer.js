065f649a2d538889a12fffec7a49bf7358bf9d25
kali-red-tylbf <yana.sutton@ace.tamut.edu>
2026-05-04 02:11:32 -0500

    Ran Test-Completed Assignment

--- lesson-05-terminal-commands/level-07/answer.js
+++ lesson-05-terminal-commands/level-07/answer.js
@@ -1,3 +1,3 @@
 // Level 07 — Command history (Up/Down arrows)
 // TODO: Use Up/Down or Ctrl+R and write a short note here mentioning "up"/"down" or "ctrl+r".
-export default "";
+export default "I saw the commands changing as I pressed the up/down arrow. The other command messed up my entire screen when I tried it.";
