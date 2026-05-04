c16758ff152466dceed90fc95c3f75cd5c7b9364
kali-red-tylbf <yana.sutton@ace.tamut.edu>
2026-05-04 00:06:05 -0500

    Ran Test Made an Update

--- lesson-05-terminal-commands/level-02/answer.js
+++ lesson-05-terminal-commands/level-02/answer.js
@@ -1,3 +1,3 @@
 // Level 02 — List files with ls
 // TODO: Run `ls` in the terminal and write a short note here about what you saw.
-export default "I ran Ls and I can see 9 folders in the directory.";
+export default "I ran Ls and I can see 9 folders in the directory, README.md and test-messages.min.js are the last 2 items.";
