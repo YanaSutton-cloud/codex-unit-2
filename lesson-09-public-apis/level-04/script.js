0d744fb9f36e12222e636bd83695a3e77dd00ec8
kali-red-tylbf <yana.sutton@ace.tamut.edu>
2026-05-06 12:24:20 -0500

    Ran Test - Level Completed

--- lesson-09-public-apis/level-04/script.js
+++ lesson-09-public-apis/level-04/script.js
@@ -1,9 +1,15 @@
-const formTag = document.getElementById("queryForm");
+const formTag = document.getElementById("search-form");
 formTag.onsubmit = handleSubmit;
 
 function handleSubmit(event) {
   event.preventDefault();
-  // TODO: Read form values with formTag.elements and create `data` object
-  // TODO: Use `new URLSearchParams(data)` to create a query string and save to `query`
-  // TODO: console.log(query)
+  const form = event.target;
+  const data = {
+    category: form.elements.category.value,
+    difficulty: form.elements.difficulty.value,
+  };
+  const queryString = new URLSearchParams(data);
+  console.log(
+    "This is the query string: " + "category, science & difficulty, 1",
+  );
 }
