955bddf11e18fb84ee644baf44efc6f5cfc0d230
kali-red-tylbf <yana.sutton@ace.tamut.edu>
2026-05-06 10:54:33 -0500

    Ran Test - Level Completed

--- lesson-09-public-apis/level-03/script.js
+++ lesson-09-public-apis/level-03/script.js
@@ -4,6 +4,7 @@ const errorEl2 = document.getElementById("error");
 const successEl2 = document.getElementById("success");
 
 async function handlesubmit(event) {
+  try {
     event.preventDefault();
     const formTag = event.target;
     const data = {
@@ -12,24 +13,28 @@ async function handlesubmit(event) {
     };
     const dataString = JSON.stringify(data);
     const response = await fetch("https://dummyjson.com/auth/login", {
-      method: "POST",
+      method: "GET",
       body: dataString,
       headers: { "Content-Type": "application/json" },
-});
-const result = await response.json();
-const message = result.message;
-const firstName = result.firstname;
-const errorTag = document.getElementById("error");
-const successTag = document.getElementById("success");
-if (message) {
-  errorTag.innerText = message;
-  successTag.innerText = "";
-} else if (firstName) {
-successTag.innerText = "You are logged in as" + firstname;
-errorTag.innerText = "";
-formTag.reset();
+    });
+    const result = await response.json();
+    const message = result.message;
+    const firstName = result.firstname;
+    const errorTag = document.getElementById("error");
+    const successTag = document.getElementById("success");
+    if (message) {
+      errorTag.innerText = message;
+      successTag.innerText = "";
+    } else if (firstName) {
+      successTag.innerText = "You are logged in as" + firstname;
+      errorTag.innerText = "";
+      formTag.reset();
+    }
+  } catch (error) {
+    debugger;
+
+    console.error("You need to use the POST method.");
+    const errorTag = document.getElementById("error");
+    errorTag.innerText = "There's an error connecting to the server.";
+  }
 }
-} catch (error) {
-  console.error("You need to use the POST method.");
-  const errorTag = document.getElementById("error");
- } 
