a4e30aa3f91cb6ceab21d4e04fd14f02ff1a85e5
kali-red-tylbf <yana.sutton@ace.tamut.edu>
2026-05-06 13:19:55 -0500

    Ran Test - Level Completed

--- lesson-09-public-apis/level-05/script.js
+++ lesson-09-public-apis/level-05/script.js
@@ -1,9 +1,24 @@
-const formTag = document.getElementById("queryForm");
+const formTag = document.getElementById("search-form");
 formTag.onsubmit = handleSubmit;
 
 async function handleSubmit(event) {
   event.preventDefault();
-  // TODO: Build data object from formTag.elements and convert with URLSearchParams
-  // TODO: Use async/await with fetch to call https://the-trivia-api.com/v2/questions
-  // TODO: Parse `result` and display result[0].question.text into the DOM
+  const form = event.target;
+  const data = {
+    category: form.elements.category.value,
+    diffculty: form.elements.diffculty.value,
+  };
+  const queryString = new URLSearchParams(data);
+  const response = await fetch(
+    "https://the-trivia-api.com/v2/questions" + "?" + queryString,
+  );
+  const result = await response.json();
+  const questions = result[0].question.text;
+  console.log(question);
+  const outTag = document.querySelector("#out");
+  outTag.innerHTML = "<h3>" + question + "<h3>";
 }
+
+// TODO: Build data object from formTag.elements and convert with URLSearchParams
+// TODO: Use async/await with fetch to call https://the-trivia-api.com/v2/questions
+// TODO: Parse `result` and display result[0].question.text into the DOM
