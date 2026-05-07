3acefeeaf5296d47b560833fb0ed7821509c938a
kali-red-tylbf <yana.sutton@ace.tamut.edu>
2026-05-07 05:39:22 -0500

    Finished Assignment - Ran Test

--- lesson-07-browser-objects/level-09-event-target/script.js
+++ lesson-07-browser-objects/level-09-event-target/script.js
@@ -1,11 +1,17 @@
 // Level 09 — event.target starter
 // TODO: inside submit handler inspect event.target and event.currentTarget
+const form = document.getElementById("sample-form");
+form.onsubmit = handleSubmit;
 
 function handleSubmit(event) {
+  event.preventDefault();
+  const formTag = event.target;
   // TODO: call event.preventDefault() when experimenting
   // event.preventDefault();
+  console.log(formTag.id);
   console.log("event.target:", event.target);
   console.log("event.currentTarget:", event.currentTarget);
+
   // TODO: const form = event.target; use form.elements to access inputs
 }
 
