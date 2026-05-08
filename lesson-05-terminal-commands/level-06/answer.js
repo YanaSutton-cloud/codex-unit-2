56c2b5c4eae97212cd9c311d271b70a9e4be96be
kali-red-tylbf <yana.sutton@ace.tamut.edu>
2026-05-07 21:44:35 -0500

    Made Corrections - Ran Test

--- lesson-05-terminal-commands/level-06/answer.js
+++ lesson-05-terminal-commands/level-06/answer.js
@@ -1,3 +1,3 @@
 // Level 06 — Run Node.js files with node
 // TODO: Run `node hello.js` and write a short note here mentioning `node`.
-export default "I ran node hello.js and the code said the MODULE_NOT_FOUND.";
+export default "I ran node hello.js and it displayed const form = document.getElementById("login-form"); with some other information";
