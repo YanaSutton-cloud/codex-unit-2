e17294886dbdf328c86c9bc0dc78d7f127322e07
kali-red-tylbf <yana.sutton@ace.tamut.edu>
2026-05-08 21:56:03 -0500

    Finished Assignment - Ran Test

--- lesson-08-rest-api/level-01/script.js
+++ lesson-08-rest-api/level-01/script.js
@@ -1,4 +1,6 @@
 // Level 01 starter script
 // TODO: Select a target element and set its innerText to explain REST API.
-const result = document.getElementById("result");
+const pTag = document.getElementById("result");
+pTag.innerText =
+  "A REST API is a way for my web page to communicate with servers, to get data like directions, weather forcast, and securly process a payment.";
 // Example: result.innerText = '...'; // fill this in during the exercise
