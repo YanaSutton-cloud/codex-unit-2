860bbfb241472c2a37c6ae7999c1288b4c311a7a
kali-red-tylbf <yana.sutton@ace.tamut.edu>
2026-05-15 17:37:11 -0500

    Made Changes

--- lesson-07-browser-objects/level-04-innerHTML/script.js
+++ lesson-07-browser-objects/level-04-innerHTML/script.js
@@ -1,2 +1,6 @@
 // TODO: Select element with getElementById('content'), log previous innerHTML, then set new innerHTML.
 // Keep changes simple and avoid inserting untrusted markup.
+const divTag = document.getElementById("content");
+
+console.log(divTag.innerHTML);
+divTag.innerHTML = "<h1>Hello World!</h1>";
