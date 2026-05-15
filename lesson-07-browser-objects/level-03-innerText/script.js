2f49ca5c382b2c824e0a578fef7eca369b127e08
kali-red-tylbf <yana.sutton@ace.tamut.edu>
2026-05-15 17:23:34 -0500

    Made Changes

--- lesson-07-browser-objects/level-03-innerText/script.js
+++ lesson-07-browser-objects/level-03-innerText/script.js
@@ -2,3 +2,8 @@
 // Example starter:
 // const element = document.getElementById('desc');
 // console.log('before:', element.innerText);
+const pTag = document.getElementById("desc");
+
+console.log(pTag.innerText);
+pTag.innerText = "Hello World!";
+console.log(pTag.innerText);
