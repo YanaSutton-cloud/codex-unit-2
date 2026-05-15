80bd76442e8466e407598b9b74f55c99eb3676d6
kali-red-tylbf <yana.sutton@ace.tamut.edu>
2026-05-14 21:29:24 -0500

    Finished Assignment - Ran Test

--- lesson-08-rest-api/level-05/script.js
+++ lesson-08-rest-api/level-05/script.js
@@ -1,13 +1,19 @@
 // Level 05 starter script
 // TODO: Fetch GET https://api.jsoning.com/mock/public/users, await response.json(), and read display the data.
 const form = document.getElementById("sample-form");
-const pUsername = document.getElementById("username");
-const pEmail = document.getElementById("email");
-const pId = document.getElementById("id");
+form.onsubmit = handleSubmit;
 
 async function handleSubmit(e) {
   e.preventDefault();
-  // TODO: implement fetch and set the three elements' innerText
+  response = await fetch("https://jsonplaceholder.typicode.com/todos/1");
+  const result = await response.json();
+  const userId = userdata.userId;
+  const title = userdata.title;
+  const isCompleted = userdata.Completed;
+  const pTag1 = document.getElementById("username");
+  const pTag2 = document.getElementById("email");
+  const pTag3 = document.getElementById("id");
+  pTag1.innerText = "User ID: " + userId;
+  pTag2.innerText = "Title: " + title;
+  pTag3.innerText = "Completed: " + isCompleted;
 }
-
-if (form) form.onsubmit = handleSubmit;
