dc162a2abb2edafa15cc7287fb8377050c871f04
kali-red-tylbf <yana.sutton@ace.tamut.edu>
2026-05-15 09:43:57 -0500

    Finished Assignment - Ran Test

--- lesson-08-rest-api/level-08/script.js
+++ lesson-08-rest-api/level-08/script.js
@@ -1,12 +1,22 @@
 // Level 07 starter script
 // TODO: Build a data object from form.elements and use fetch POST to https://api.jsoning.com/mock/public/users
 const form = document.getElementById("sample-form");
-const result = document.getElementById("result");
+form.onsubmit = handleSubmit;
 
-async function handleSubmit(e) {
-  e.preventDefault();
-  // TODO: collect values via form.elements[0].value, form.elements[1].value, form.elements[2].value
-  // build a data object and post it with fetch; then set result.innerText explaining how data is attached
+async function handleSubmit(event) {
+  event.preventDefault();
+  const form = event.target;
+  const data = {
+    a: form.elements.a.valur,
+    b: form.elements.b.value,
+    c: form.elements.c.value,
+  };
+  const response = await fetch("https://api.jsoning.com/mock/public/users", {
+    method: "POST",
+    body: JSON.stringify(data),
+  });
+
+  const pTag = document.getElementById("result");
+  pTag.innerText =
+    "Provide an options object. The method property should be POST body propertybcontains the data.";
 }
-
-if (form) form.onsubmit = handleSubmit;
