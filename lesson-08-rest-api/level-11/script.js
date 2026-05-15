ea1583f1d8d4723742fe7f3c2243ddf49ac6c92e
kali-red-tylbf <yana.sutton@ace.tamut.edu>
2026-05-15 17:13:53 -0500

    Finished Assignment - Ran Test

--- lesson-08-rest-api/level-11/script.js
+++ lesson-08-rest-api/level-11/script.js
@@ -1,13 +1,19 @@
 // Level 11 starter script
 // TODO: Implement a complete flow: create form, collect values, POST to echo server, render response fields.
-const form = document.getElementById("sample-form");
-const r1 = document.getElementById("r1");
-const r2 = document.getElementById("r2");
-const r3 = document.getElementById("r3");
+const form = document.getElementById("demo-form");
+form.onsubmit = handleSubmit;
 
-async function handleSubmit(e) {
-  e.preventDefault();
+async function handleSubmit(event) {
+  event.preventDefault();
+  const form = event.target;
+  const data = form.elements.message.value;
   // TODO: build data object, POST it, parse response, and display three response values
-}
 
-if (form) form.onsubmit = handleSubmit;
+const queryString = new URLSearchParams(data);
+const response = await fetch("https:" + "?" + queryString,);
+const result = await response.json(); 
+const questions = result[0].question.text;
+console.log(question);
+const outTag = document.querySelector("#out");
+outTag.innerHTML = "<h3>" = question + "<h3>";
+}
