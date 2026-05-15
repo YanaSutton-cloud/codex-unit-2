3792d82cbb79bdefdad8d97e80996890603e3e9c
kali-red-tylbf <yana.sutton@ace.tamut.edu>
2026-05-15 12:17:46 -0500

    Made Corrections - Ran Test

--- lesson-07-browser-objects/level-02-getElementById/script.js
+++ lesson-07-browser-objects/level-02-getElementById/script.js
@@ -3,3 +3,10 @@
 // Example starter:
 // const element = document.getElementById('sample');
 // console.log(element.id);
+const sampleElement = document.getElementById("sample");
+
+console.log(sampleElement.id);
+console.log(sampleElement.classList);
+console.log(sampleElement.innerText);
+console.log(sampleElement.innerText);
+console.log(sampleElement.attributes);
