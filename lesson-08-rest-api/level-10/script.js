0bc605cea63f60f0358a2a854098eb6435a64aa1
kali-red-tylbf <yana.sutton@ace.tamut.edu>
2026-05-15 10:14:32 -0500

    Finished Assignment - Ran Test

--- lesson-08-rest-api/level-10/script.js
+++ lesson-08-rest-api/level-10/script.js
@@ -1,13 +1,26 @@
 // Level 10 starter script
 // TODO: POST pre-processed data, parse JSON response, and display three selected values.
 const form = document.getElementById("sample-form");
+form.onsubmit = handleSubmit;
+
 const v1 = document.getElementById("v1");
 const v2 = document.getElementById("v2");
 const v3 = document.getElementById("v3");
 
-async function handleSubmit(e) {
-  e.preventDefault();
+async function handleSubmit(event) {
+  event.preventDefault();
+  const data = {
+    a: form.elements.a.value,
+    b: form.elements.b.value,
+    c: form.elements.c.value,
+  };
+  const response = await fetch("https://api.jsoning.com/mock/public/users", {
+    method: "POST",
+    body: JSON.stringify(data),
+  });
+  const user = await response.json();
+  const id = user.id;
+  const pTag = document.getElementById("v1");
+  pTag.innerText = "user ID: " + id;
   // TODO: collect data, POST to echo server, parse response, and insert values into v1/v2/v3
 }
-
-if (form) form.onsubmit = handleSubmit;
