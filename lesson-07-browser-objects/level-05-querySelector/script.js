a99d82063ee14c01f7fcd2666ecdbc7d5bb6faa3
kali-red-tylbf <yana.sutton@ace.tamut.edu>
2026-05-15 17:51:30 -0500

    Made Changes

--- lesson-07-browser-objects/level-05-querySelector/script.js
+++ lesson-07-browser-objects/level-05-querySelector/script.js
@@ -1,1 +1,4 @@
 // TODO: Use querySelector('#content') to select element and update innerHTML to explain querySelector vs getElementById
+const liTag = document.querySelector("#three");
+
+liTag.innerHTML = "<h2>Hello World!</h2>";
