9ea9501035818346273bc096fd0a11029280c1fd
kali-red-tylbf <yana.sutton@ace.tamut.edu>
2026-05-16 18:27:49 -0500

    Made Changes

--- lesson-09-public-apis/level-02/script.js
+++ lesson-09-public-apis/level-02/script.js
@@ -1,32 +1,32 @@
 const form = document.getElementById("login-form");
 form.onsubmit = handleSubmit;
-const errorEl2 = document.getElementById("error");
-const successEl2 = document.getElementById("success");
+const errorEl = document.getElementById("error");
+const successEl = document.getElementById("success");
 
 async function handleSubmit(event) {
-  
-event.preventDefault();
-const formTag = event.target;
-const data = {
-  username: formTag.elements.username.value,
-  password: formTag.elements.password.value,
-};
-const dataString = JSON.stringify(data);
-const response = await fetch("https://dummyjson.com/auth/login", {
- method: "POST",
- body: dataString,
- headers: { "Content-Type": "applicatiom/json" },
-});
-const result = await response.json();
-const message = result.message;
-const firstName = result.firstName;
-const errorTag = document.getElementById("error");
-const successTag = document.getElementById("success");
-if (message) {
-  errorTag.innerText = message;
-  successTag.innerText = "";
-} else if (firstName) {
-  successTag.innerText = "You are logged in as " + firstName;
-  errorTag.innerText = "";
-  formTag.reset();
-} 
+  event.preventDefault();
+  const formTag = event.target;
+  const data = {
+    username: formTag.elements.username.value,
+    password: formTag.elements.password.value,
+  };
+  const dataString = JSON.stringify(data);
+  const response = await fetch("https://dummyjson.com/auth/login", {
+    method: "POST",
+    body: dataString,
+    headers: { "Content-Type": "application/json" },
+  });
+  const result = await response.json();
+  const message = result.message;
+  const firstName = result.firstName;
+  const errorTag = document.getElementById("error");
+  const successTag = document.getElementById("success");
+  if (message !== undefined) {
+    errorEl.innerText = "Invalid Credentials";
+    successTag.innerText = "";
+  } else if (firstName !== undefined) {
+    successEl.innerText = "You are logged in as " + firstName;
+    errorTag.innerText = "";
+    formTag.reset();
+  }
+}
