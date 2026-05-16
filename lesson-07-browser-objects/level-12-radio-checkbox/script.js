6cbc45c6051601793ed9e1b1beeb9e0af00c1f25
kali-red-tylbf <yana.sutton@ace.tamut.edu>
2026-05-15 20:54:34 -0500

    Finished Assignment

--- lesson-07-browser-objects/level-12-radio-checkbox/script.js
+++ lesson-07-browser-objects/level-12-radio-checkbox/script.js
@@ -3,3 +3,8 @@
 // const form = document.getElementById('sample-form');
 // const color = form.elements.color.value;
 // const isSubscribed = form.elements.subscribe.checked;
+const form = document.getElementById("sample-form");
+form.onsubmit = handleSubmit;
+
+const color = form.nextElementSibling.color.value;
+const isSubscribed = form.nextElementSibling.subscribe.checked;
