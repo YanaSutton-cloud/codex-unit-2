0a862d6e20b471c33e1b96de00adb6c16644af09
kali-red-tylbf <yana.sutton@ace.tamut.edu>
2026-05-17 01:40:27 -0500

    Finished Level -Made Changes - Ran Test

--- lesson-09-public-apis/level-07/script.js
+++ lesson-09-public-apis/level-07/script.js
@@ -2,6 +2,12 @@ const divTag = document.getElementById("message");
 divTag.innerHTML =
   "<h4>An API Key is a unique code that must be attached to each request to the API server.</h4>";
 
+const data = {
+  api: "5f71845b4c384bd39d85b1e858bf218a",
+};
+const queryString = new URLSearchParams(data);
+fetch("http://url.com" + "?" + "api=5f71845b4c384bd39d85b1e858bf218a");
+
 if (messageEl) {
   messageEl.innerText =
     "API keys identify your app to a service; do not commit them publicly.";
