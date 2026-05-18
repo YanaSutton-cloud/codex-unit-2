fb00b6dbdcebf08724673f71ffc1885e3a4f8373
kali-red-tylbf <yana.sutton@ace.tamut.edu>
2026-05-18 16:57:26 -0500

    Finished Assignment - Ran Test

--- lesson-04-js-objects/level-02/examples.js
+++ lesson-04-js-objects/level-02/examples.js
@@ -1,5 +1,5 @@
 // TODO: Default-export an array of two strings contrasting objects with primitives and arrays.
 export default [
-  "Objects hold named keys mapping to values; primitives hold a single value.",
-  "Arrays are ordered lists accessed by index; objects use named keys for lookup.",
+  "Primitives hold 1 CSSMathValue. Objects can hold many values.",
+  "Items in an arry are accessed wuth an index number. Values in an object are accessed by name.",
 ];
