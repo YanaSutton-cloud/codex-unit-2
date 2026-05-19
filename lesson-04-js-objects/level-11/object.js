317456ac6b460b569f13f9bfff8e72120c8a4c0a
kali-red-tylbf <yana.sutton@ace.tamut.edu>
2026-05-19 18:35:46 -0500

    Finished Assignment - Ran Test

--- lesson-04-js-objects/level-11/object.js
+++ lesson-04-js-objects/level-11/object.js
@@ -1,6 +1,15 @@
 // TODO: Build an object from a written specification and export it as default.
 // Example spec: book with title, author, pages, and a summary() method assigned from a named function.
+function getSummary() {
+  return `${this.title} by ${this.author}, ${this.pages} pages.`;
+}
+
 const book = {
-  /* TODO: implement spec */
+  title: "The Sisters of the APF",
+  author: "Zane",
+  pages: 288,
+
+  summary: getSummary,
 };
+
 export default book;
