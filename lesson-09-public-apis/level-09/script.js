5caafe3d34b67debd5ba54995448d1b56ff2be09
kali-red-tylbf <yana.sutton@ace.tamut.edu>
2026-05-22 02:30:57 -0500

    Made Changes - Ran Test

--- lesson-09-public-apis/level-09/script.js
+++ lesson-09-public-apis/level-09/script.js
@@ -1,10 +1,24 @@
 const formTag = document.getElementById("challengeForm");
 formTag.onsubmit = handleSubmit;
 
-function handleSubmit(event) {
+async function handleSubmit(event) {
   event.preventDefault();
-  // TODO: Construct `data` from formTag.elements
-  // TODO: Format data with `URLSearchParams` or `JSON.stringify`
-  // TODO: Save formatted data into `dataString` and send via fetch using async/await
-  // TODO: Parse `result` and update the page; handle errors with try/catch
+  const form = event.target;
+  const data = form.elements.meme.value;
+
+  const data = { "api-key": "" };
+  const queryString = new URLSearchParams(data);
+  const respone = await fetch(url + "?" + queryString + "api=");
+  const result = await response.json();
+  const description = result.description;
+  const src = result.url;
+  const imageTag = document.getElementById("meme");
+  const outputTag = document.getElementById("description");
+  outputTag.innerText = description;
+  imageTag.src = src;
 }
+
+// TODO: Construct `data` from formTag.elements
+// TODO: Format data with `URLSearchParams` or `JSON.stringify`
+// TODO: Save formatted data into `dataString` and send via fetch using async/await
+// TODO: Parse `result` and update the page; handle errors with try/catch
