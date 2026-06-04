ab87513d27dccf21d83a7809ff6834a74d4d2277
kali-red-tylbf <yana.sutton@ace.tamut.edu>
2026-06-04 16:59:54 -0500

    Completed Assignment - Ran Test

--- lesson-06-js-syntax/level-03-practice-variables/script.js
+++ lesson-06-js-syntax/level-03-practice-variables/script.js
@@ -7,16 +7,22 @@ let userAge;
 let city;
 let country;
 
+firstName = "Yana";
+lastName = "Sutton";
+userAge = 45;
+city = "New Orleans";
+country = "United States";
+
 // TODO: Part B: fix five faulty string initializations so they parse correctly.
 
-let = "noName";
+let firstName = "noName";
 
-let lastName "Smith";
+let lastName = "Smith";
 
-let city = "New York;
+let city = "New York";
 
-let country = ""USA"";
+let country = "USA";
 
-let planet = Earth;
+let planet = "Earth";
 
-export default firstame;
+export default firstName;
