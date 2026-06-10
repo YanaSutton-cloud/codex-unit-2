eb0d9497810c25e18ccf6bc49eb3a2885e526e5b
kali-red-tylbf <yana.sutton@ace.tamut.edu>
2026-06-10 07:18:10 -0500

    Completed Assignment - Made Changes

--- lesson-06-js-syntax/level-07-declare-objects/script.js
+++ lesson-06-js-syntax/level-07-declare-objects/script.js
@@ -4,6 +4,32 @@
 // Example:
 // let userProfile = { name: "Ava", age: 28, isSubscriber: false };
 
-let userProfile = {};
+let userProfile = { name: "Yana", age: "45", isSubscriber: true };
+
+let car = { make: "Audi", model: "Q3", year: "2021" };
+
+let laptop = { brand: "MicroSoft", model: "Surface Pro", storageTB: "1" };
+
+let movie = { title: "Michael", director: "Antoine Fuqa", releaseYear: "2026" };
+
+let hotel = { name: "The Ritz-Carlton", location: "New Orleans", stars: "5" };
+
+let pet = { species: "dog", hasFur: "true", name: "Beau" };
+
+let smartphone = {
+  brand: "Samsung",
+  model: "Galaxy S26 Ultra",
+  cameraMegapixels: "200",
+};
+
+let airlineFlight = { flightNumber: "DL1234", depature: "MSY", arrival: "ATL" };
+
+let recipe = {
+  name: "Spaghetti with Meat Sauce",
+  prepTimeMinutes: "20",
+  isVegetarian: "false",
+};
+
+let book = { title: "48 Laws of Power", author: "Robert Greene", pages: "956" };
 
 export default userProfile;
