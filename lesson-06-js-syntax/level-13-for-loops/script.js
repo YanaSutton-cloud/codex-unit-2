6f64a96cfcec313975e309ef878e800a8e9c5ca7
kali-red-tylbf <yana.sutton@ace.tamut.edu>
2026-06-11 05:41:45 -0500

    Completed Assignment - Ran Test

--- lesson-06-js-syntax/level-13-for-loops/script.js
+++ lesson-06-js-syntax/level-13-for-loops/script.js
@@ -1,2 +1,50 @@
 // Level 13 — `for` loops practice (10 different ranges)
 // TODO: Write 10 different for loops that print iterated numbers with console.log.
+
+for (let i = 0; i < 10; i++) {
+  console.log(i);
+}
+
+for (let count = 2; count <= 10; count = count + 2) {
+  console.log(count);
+}
+
+for (let index = 3; index > 7; index++) {
+  console.log(index);
+}
+
+for (let i = 1; i < Infinity; i++) {
+  console.log("I love you");
+}
+
+for (let i = 1; i <= 5; i++) {
+  console.log(i);
+}
+
+for (let i = 0; i < 3; i++) {
+  console.log(i);
+}
+
+for (let i = 10; i >= 1; i--) {
+  console.log(i);
+}
+
+for (let i = 0; i <= 10; i += 2) {
+  console.log(i);
+}
+
+for (let i = 20; i < 25; i++) {
+  console.log(i);
+}
+
+for (let i = -1; i >= -7; i--) {
+  console.log(i);
+}
+
+for (let i = 0; i <= 20; i += 2) {
+  console.log(i);
+}
+
+for (let i = 100; i >= 0; i -= 10) {
+  console.log(i);
+}
