c7982eeede3ea93dd6f728689b18eed69a0365aa
kali-red-tylbf <yana.sutton@ace.tamut.edu>
2026-06-11 03:52:58 -0500

    Completed Assignment - Ran Test

--- lesson-06-js-syntax/level-11-function-calls/script.js
+++ lesson-06-js-syntax/level-11-function-calls/script.js
@@ -6,3 +6,14 @@ export function reportScore(playerName, score) {
 }
 
 const avaScoreReport = reportScore("Ava", 0);
+const yanaScore = reportScore("Yana", 777);
+const amazenScore = reportScore("AmaZen", 77);
+const elaineScore = reportScore("Elaine", 7777);
+const beauScore = reportScore("Beau", 7);
+const julietteScore = reportScore("Juliette", 8888);
+const huongScore = reportScore("Huong", 888);
+const roseScore = reportScore("Rose", 88);
+const upendoScore = reportScore("Upendo", 8);
+const marthaScore = reportScore("Martha", 33);
+
+export default amazenScore;
