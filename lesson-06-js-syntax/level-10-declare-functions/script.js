e5a19599e9229c56b084db601ebc4b8276315f84
kali-red-tylbf <yana.sutton@ace.tamut.edu>
2026-06-11 03:29:15 -0500

    Completed Assignment - Ran Test

--- lesson-06-js-syntax/level-10-declare-functions/script.js
+++ lesson-06-js-syntax/level-10-declare-functions/script.js
@@ -4,3 +4,41 @@
 function getUser() {
   return "returns the current user";
 }
+
+function getFirstName() {
+  return "Yana";
+}
+
+function getLastName() {
+  return "Sutton";
+}
+
+function getUserGreeting() {
+  return "Welcome Back Yana Sutton!";
+}
+
+function sayHi() {
+  return "Hello!";
+}
+
+function cancelOrder() {
+  return "cancels the current order";
+}
+
+function addToCart() {
+  return "adds an items to cart";
+}
+
+function calculateTotal() {
+  return "price + tax";
+}
+
+function getWeather() {
+  return "Partly cloudly with a high of 82 degrees";
+}
+
+function depositCheck() {
+  return "deposits a check remotely";
+}
+
+export default sayHi;
