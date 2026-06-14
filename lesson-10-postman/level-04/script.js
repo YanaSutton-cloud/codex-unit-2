7ae0f404f18e1e75462ec88e1b19b2b477816f29
kali-red-tylbf <yana.sutton@ace.tamut.edu>
2026-06-13 23:03:04 -0500

    Completed Assignment

--- lesson-10-postman/level-04/script.js
+++ lesson-10-postman/level-04/script.js
@@ -1,4 +1,6 @@
 // TODO: Starter for Level 04: POST request basics.
 
 // Students should use Postman to send a POST; this file is a placeholder
-console.log("TODO: log one documentation fact here");
+console.log(
+  "One fact I learned about POST request is that they are used to send data to a special URL, and the data is included in the body of the reuest.",
+);
