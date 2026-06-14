ddfb6ca223b3204e6e978d63264f214c9da09a9c
kali-red-tylbf <yana.sutton@ace.tamut.edu>
2026-06-14 04:26:21 -0500

    Made Correction

--- lesson-10-postman/level-07/script.js
+++ lesson-10-postman/level-07/script.js
@@ -5,20 +5,7 @@ console.log(
   "Payload data is the data sent between a client and server during a request or response. JSON payloads are a way to send data to an API in a structured format.",
 );
 
-[
-  {
-    q: "Lack of emotion causes lack of progress and lack of motivation.",
-    a: "Tony Robbins",
-    i: "https://zenquotes.io/img/tony-robbins.jpg",
-    c: "63",
-    h: "<blockquote>&ldquo;Lack of emotion causes lack of progress and lack of motivation.&rdquo; &mdash; <footer>Tony Robbins</footer></blockquote>",
-  },
-  // ...MORE DATA... //
-  {
-    q: "The friend is the man who knows all about you, and still likes you.",
-    a: "Elbert Hubbard",
-    i: "https://zenquotes.io/img/elbert-hubbard.jpg",
-    c: "67",
-    h: "<blockquote>&ldquo;The friend is the man who knows all about you, and still likes you.&rdquo; &mdash; <footer>Elbert Hubbard</footer></blockquote>",
-  },
-];
+https://reqbin.com/echo/post/json
+
+{"success":"true"};
+
