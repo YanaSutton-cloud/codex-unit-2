cf2ad8ee363d71572a1d45cc867a22e2338a50bb
kali-red-tylbf <yana.sutton@ace.tamut.edu>
2026-06-13 23:58:17 -0500

    Completed Assignment

--- lesson-10-postman/level-05/script.js
+++ lesson-10-postman/level-05/script.js
@@ -1,4 +1,6 @@
 // TODO: Starter for Level 05: POST with JSON payloads.
 
 // Placeholder: students should set Content-Type and send JSON in Postman.
-console.log("TODO: log one documentation fact here");
+console.log(
+  "Using the POST method with JSON payloads requires setting the content type to application/json and sending the data in JSON format to get the proper responses.",
+);
