ceddfdb3d4b3d9b2f88f1cd0d42efa6df2bc4c57
kali-red-tylbf <yana.sutton@ace.tamut.edu>
2026-06-13 22:17:33 -0500

    Completed Assignment

--- lesson-10-postman/level-02/script.js
+++ lesson-10-postman/level-02/script.js
@@ -2,4 +2,6 @@
 // Example: Console.log one fact learned from the API docs.
 
 // TODO: Replace this placeholder with a short note from the docs.
-console.log("TODO: log one documentation fact here");
+console.log(
+  "The docs show there are different ways to get sample users and trivia questions.",
+);
