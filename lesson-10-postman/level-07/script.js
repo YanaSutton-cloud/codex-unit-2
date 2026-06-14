d117e8783354e0b18d2858f5512ae0519290a592
kali-red-tylbf <yana.sutton@ace.tamut.edu>
2026-06-14 01:09:34 -0500

    Completed Assignment

--- lesson-10-postman/level-07/script.js
+++ lesson-10-postman/level-07/script.js
@@ -1,4 +1,24 @@
 // TODO: Starter for Level 07: API POST practice.
 
 // Students should construct a JSON payload and send it to their chosen API.
-console.log("TODO: log one documentation fact here");
+console.log(
+  "Payload data is the data sent between a client and server during a request or response. JSON payloads are a way to send data to an API in a structured format.",
+);
+
+[
+  {
+    q: "Lack of emotion causes lack of progress and lack of motivation.",
+    a: "Tony Robbins",
+    i: "https://zenquotes.io/img/tony-robbins.jpg",
+    c: "63",
+    h: "<blockquote>&ldquo;Lack of emotion causes lack of progress and lack of motivation.&rdquo; &mdash; <footer>Tony Robbins</footer></blockquote>",
+  },
+  // ...MORE DATA... //
+  {
+    q: "The friend is the man who knows all about you, and still likes you.",
+    a: "Elbert Hubbard",
+    i: "https://zenquotes.io/img/elbert-hubbard.jpg",
+    c: "67",
+    h: "<blockquote>&ldquo;The friend is the man who knows all about you, and still likes you.&rdquo; &mdash; <footer>Elbert Hubbard</footer></blockquote>",
+  },
+];
