4682324234c3aeb87d88c8adb27b352c2e74c253
kali-red-tylbf <yana.sutton@ace.tamut.edu>
2026-06-14 00:37:30 -0500

    Completed Assignment

--- lesson-10-postman/level-06/script.js
+++ lesson-10-postman/level-06/script.js
@@ -2,4 +2,19 @@
 
 // Students should replace the URL below with their chosen API endpoint.
 // TODO: fetch('https://api.example.com/endpoint')
-console.log("TODO: log one documentation fact here");
+
+fetch("https://zenquotes.io/api/quotes/endpoint");
+
+console.log(
+  "I learned that it's important to use the correct URL for the API endpoint, and that I can use Postman to test my API calls before writing code. I also learned that APIs can return data in different formats.",
+);
+
+const api_url = "https://zenquotes.io/api/quotes/";
+
+async function getapi(url) {
+  const response = await fetch(url);
+  var data = await response.json();
+  console.log(data);
+}
+
+getapi(api_url);
